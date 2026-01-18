335fe7126ddfbfa495f756921e661d70cdd1e17e
Derek K <derek.kissoon@gmail.com>
2026-01-18 17:25:28 -0500

    Initial assignment submission

---
+++ M1.py
@@ -0,0 +1,9 @@
+# Greeting with name
+name = "Derek"
+print(f"Hello, my name is {name} and I am learning Python.")
+
+# Small calculation: adding two numbers
+num1 = 5
+num2 = 7
+result = num1 * num2
+print(f"{num1} * {num2} = {result}")
